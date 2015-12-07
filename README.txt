194d51660cdc25dcd6347aac1b3a1b23d432ed94
Michelle <michelle.murphy186@gmail.com>
2015-12-06 22:55:27 -0500

    First commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/anon/pen/adzopg.
+
+ 
+
+Forked from [Mario Vidov](http://codepen.io/mel/)'s Pen [jQuery Picture Memory Game](http://codepen.io/mel/pen/Brads/).
---
+++ js/index.js
@@ -0,0 +1,112 @@
+var BoxOpened = "";
+var ImgOpened = "";
+var Counter = 0;
+var ImgFound = 0;
+
+var Source = "#boxcard";
+
+var ImgSource = [
+  "images/gronk01.png",
+  "images/gronk02.png",
+  "images/gronk03.jpg",
+  "images/gronk04.jpg",
+  "images/gronk05.jpg",
+  "images/gronk06.jpg",
+  "images/gronk07.jpg",
+  "images/gronk08.jpg",
+  "images/gronk09.jpeg",
+  "images/gronk10.jpg"
+];
+
+function RandomFunction(MaxValue, MinValue) {
+		return Math.round(Math.random() * (MaxValue - MinValue) + MinValue);
+	}
+	
+function ShuffleImages() {
+	var ImgAll = $(Source).children();
+	var ImgThis = $(Source + " div:first-child");
+	var ImgArr = new Array();
+
+	for (var i = 0; i < ImgAll.length; i++) {
+		ImgArr[i] = $("#" + ImgThis.attr("id") + " img").attr("src");
+		ImgThis = ImgThis.next();
+	}
+	
+		ImgThis = $(Source + " div:first-child");
+	
+	for (var z = 0; z < ImgAll.length; z++) {
+	var RandomNumber = RandomFunction(0, ImgArr.length - 1);
+
+		$("#" + ImgThis.attr("id") + " img").attr("src", ImgArr[RandomNumber]);
+		ImgArr.splice(RandomNumber, 1);
+		ImgThis = ImgThis.next();
+	}
+}
+
+function ResetGame() {
+	ShuffleImages();
+	$(Source + " div img").hide();
+	$(Source + " div").css("visibility", "visible");
+	Counter = 0;
+	$("#success").remove();
+	$("#counter").html("" + Counter);
+	BoxOpened = "";
+	ImgOpened = "";
+	ImgFound = 0;
+	return false;
+}
+
+function OpenCard() {
+	var id = $(this).attr("id");
+
+	if ($("#" + id + " img").is(":hidden")) {
+		$(Source + " div").unbind("click", OpenCard);
+	
+		$("#" + id + " img").slideDown('fast');
+
+		if (ImgOpened == "") {
+			BoxOpened = id;
+			ImgOpened = $("#" + id + " img").attr("src");
+			setTimeout(function() {
+				$(Source + " div").bind("click", OpenCard)
+			}, 300);
+		} else {
+			CurrentOpened = $("#" + id + " img").attr("src");
+			if (ImgOpened != CurrentOpened) {
+				setTimeout(function() {
+					$("#" + id + " img").slideUp('fast');
+					$("#" + BoxOpened + " img").slideUp('fast');
+					BoxOpened = "";
+					ImgOpened = "";
+				}, 400);
+			} else {
+				$("#" + id + " img").parent().css("visibility", "hidden");
+				$("#" + BoxOpened + " img").parent().css("visibility", "hidden");
+				ImgFound++;
+				BoxOpened = "";
+				ImgOpened = "";
+                alert("You got one! Gronk is proud.");
+			}
+			setTimeout(function() {
+				$(Source + " div").bind("click", OpenCard)
+			}, 400);
+		}
+		Counter++;
+		$("#counter").html("" + Counter);
+
+		if (ImgFound == ImgSource.length) {
+			$("#counter").prepend('<span id="success">You matched all of the Gronks with </span>');
+		}
+	}
+}
+
+$(function() {
+
+for (var y = 1; y < 3 ; y++) {
+	$.each(ImgSource, function(i, val) {
+		$(Source).append("<div id=card" + y + i + "><img src=" + val + " />");
+	});
+}
+	$(Source + " div").click(OpenCard);
+	ShuffleImages();
+});
